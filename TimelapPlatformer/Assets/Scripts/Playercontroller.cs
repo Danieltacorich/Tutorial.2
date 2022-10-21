22ee46a2f89469bff66d82330e274e814cd31b9c
Danieltacorich <dlopezgines1@knights.ucf.edu>
2022-10-20 23:06:54 -0400

    First Challenge

--- TimelapPlatformer/Assets/Scripts/Playercontroller.cs
+++ TimelapPlatformer/Assets/Scripts/Playercontroller.cs
@@ -9,11 +9,89 @@ public class Playercontroller : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    Animator anim;
+    private bool facingRight = true;
+    
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        //Right
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+          anim.SetInteger("State", 1);
+
+         }
+        
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+          anim.SetInteger("State", 0);
+
+         }
+
+        //Left
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+          anim.SetInteger("State", 1);
+
+         }
+         
+         if (Input.GetKeyUp(KeyCode.A))
+        {
+
+          anim.SetInteger("State", 0);
+
+         }
+
+        //Sprint
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+          anim.SetInteger("State", 2);
+
+         }
+
+         if (Input.GetKeyUp(KeyCode.R))
+        {
+          
+          anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+          anim.SetInteger("State", 3);
+
+         }
+
+         if (Input.GetKeyUp(KeyCode.W))
+        {
+          
+          anim.SetInteger("State", 0);
+        }
+        //Character Flipping
+        animator.SetFloat("HorizontalValue", Mathf.Abs(Input.GetAxis("Horizontal")));
+        animator.SetFloat("VerticalValue", Input.GetAxis("Vertical"));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
 
     }
 
@@ -32,6 +110,15 @@ public class Playercontroller : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
+    }
+
+    //Flip scaler
+     void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
